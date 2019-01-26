c0f01051f24796bb583255eaacb9c1ca5edf3731
Nuno Farinha <nadfarinha@gmail.com>
2019-01-26 14:14:56 +0000

    Test getter methods for accessors of Song class

---
+++ song.rb
@@ -0,0 +1,23 @@
+class Song
+
+  attr_reader :artist, :title, :duration, :genre, :lyrics
+
+  def initialize(artist, title, duration, genre, lyrics)
+
+    @artist = artist
+    @title = title
+    @duration = duration
+    @genre = genre
+    @lyrics = lyrics
+
+
+
+  end
+
+
+
+
+
+
+
+end
---
+++ specs/guest_specs.rb
@@ -0,0 +1,13 @@
+require("minitest/autorun")
+require("minitest/rg")
+require_relative("./guest")
+
+class TestGuest < MiniTest::Test
+
+def setup
+
+
+end
+
+
+end
